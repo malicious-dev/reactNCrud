258dd764f6773986c12e5f10f31aabbde3161a43
malwareami! <46420533+malicious-dev@users.noreply.github.com>
2022-12-28 12:35:27 +0530

    small change space

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
-import Navbar from'./components/Navbar'
-import Home from'./components/Home'
-import Register from'./components/Register';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Register from './components/Register';
 import Edit from './components/Edit'
 import View from './components/View'
 import {Routes, Route} from 'react-router-dom'
